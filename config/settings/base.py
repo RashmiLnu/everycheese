6b96943f86cf520cad82cea4cc4587835e3892eb
RashmiLnu <Rashmi.Lnu@unh.edu>
2024-02-14 11:19:51 -0500

    Add cheeses App

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,6 +79,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "cookiecutter.users.apps.UsersConfig",
+    "cookiecutter.cheeses.apps.CheesesConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
