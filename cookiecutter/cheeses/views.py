9bb1eb6b830bda41a4c12596afe8f78ddf63496b
RashmiLnu <Rashmi.Lnu@unh.edu>
2024-03-01 10:31:05 -0500

    First pass at working Add Cheese form and delete particular cheese

--- cookiecutter/cheeses/views.py
+++ cookiecutter/cheeses/views.py
@@ -1,7 +1,25 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from .models import Cheese
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+
 class CheeseListView(ListView):
     model = Cheese
 
 class CheeseDetailView(DetailView):
     model = Cheese
+
+class CheeseCreateView(CreateView):
+    model = Cheese
+    fields = [
+            "name",
+            "description",
+            "firmness",
+            "country_of_origin",
+    ]
+
+class CheeseDeleteView(DeleteView):
+    model = Cheese
+    template_name = "cheeses/cheese_confirm_delete.html"
+    success_url = reverse_lazy("cheeses:list")
